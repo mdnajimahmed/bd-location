65dd56fb2924c98a3df3ffbe213c85785696f563
Md Najim Ahmed <najim.ju@gmail.com>
2021-07-18 16:55:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,20 +10,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
-// let ALLOWED_ORIGINS = ["https://www.timelyship.com/", "https://timelyship.com/", "http://localhost:3000"];
-// app.use((req, res, next) => {
-//     let origin = req.headers.origin;
-//     console.log("request came from",origin)
-//     console.log("ALLOWED_ORIGINS",ALLOWED_ORIGINS)
-//     console.log("req.headers",req.headers)
-//     let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
-//     res.header("Access-Control-Allow-Origin", theOrigin);
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
-
-/**CORS SETUP END */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -39,11 +25,5 @@ app.get('/upazila', (req, res) => {
     res.send(bdUpazila);
 });
 
-//app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
 
-
-// (()=>{
-//     console.log("bdPostalCode",bdPostalCode)
-//     console.log("bdUpazila",bdUpazila)
-// })()
